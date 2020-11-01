9254c5bb21bd779c0bab0582253f3c7406d08b9a
amol goel <amolgoel179@gmail.com>
2020-11-01 07:49:40 +0000

    TodorestApplication.java edited online with Bitbucket

--- src/main/java/com/wf/ms/TodorestApplication.java
+++ src/main/java/com/wf/ms/TodorestApplication.java
@@ -1,10 +1,34 @@
 package com.wf.ms;
 
+import io.prometheus.client.Counter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodorestApplication {
+
+
+
+	private final Counter promRequestsTotal = Counter.build()
+	 		.name("requests total")
+			.help("Total Number Of Requests .")
+			.register();
+    
+
+    @RequestMapping(path = "/TodorestApplication")
+    public @ResponseBody String sayTodo() {
+        promRequestsTotal.inc();
+        return "Todo,Rest";
+    }
+			
+    
+    @RequestMapping(path = "/prometheus-metrics")
+    public void metrics(Writer responseWriter) throws IOException {
+        TextFormat.write004(responseWriter, CollectorRegistry.metricFamilySamples());
+        responseWriter.close();
+    }
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodorestApplication.class, args);

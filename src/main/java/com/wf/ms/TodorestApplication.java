483576c6d4d3293eac8c7fac83b6b4f0ae376e85
amol goel <amolgoel179@gmail.com>
2021-08-05 07:23:10 +0000

    TodorestApplication.java edited online with Bitbucket

--- src/main/java/com/wf/ms/TodorestApplication.java
+++ src/main/java/com/wf/ms/TodorestApplication.java
@@ -1,11 +1,11 @@
 //package com.wf.ms;
 
-//import io.prometheus.client.Counter;
+import io.prometheus.client.Counter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class TodorestApplication {
 
 	static final Counter requests = Counter.build()
